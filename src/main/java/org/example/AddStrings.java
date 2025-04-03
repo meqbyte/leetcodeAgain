3b051f39fd72e2b24d847b8b03ae616fb6f0c5d1
meqbtye <meq1986@163.com>
2025-04-03 11:10:42 +0800

    reword test

---
+++ src/main/java/org/example/AddStrings.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class AddStrings {
+    public String addStrings(String num1, String num2) {
+        int p = num1.length() - 1;
+        int q = num2.length() - 1;
+
+        int carry = 0;
+        StringBuilder result = new StringBuilder();
+        while (p >= 0 && q >= 0) {
+
+            int n1 = num1.charAt(p) - '0';
+            int n2 = num2.charAt(q) - '0';
+            int sum = (n1 + n2 + carry) % 10;
+            carry = (n1 + n2 + carry) / 10;
+
+            result.insert(0, sum);
+            p--;
+            q--;
+        }
+
+        while (p >= 0) {
+            int n1 = num1.charAt(p) - '0';
+            int sum = (n1 + carry) % 10;
+            carry = (n1  + carry) / 10;
+            p--;
+            result.insert(0, sum);
+        }
+
+        while (q >= 0) {
+            int n2 = num2.charAt(q) - '0';
+            int sum = (n2 + carry) % 10;
+            carry = (n2  + carry) / 10;
+            q--;
+            result.insert(0, sum);
+        }
+
+        return result.toString();
+
+    }
+}
