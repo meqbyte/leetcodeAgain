9f20f7e2c1f932e75284a76367b87ce6b0a96672
meqbyte <meq1986@163.com>
2025-04-08 14:03:13 +0800

    done

--- src/main/java/org/example/IsSymmetric.java
+++ src/main/java/org/example/IsSymmetric.java
@@ -4,15 +4,30 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class IsSymmetric {
-//    public boolean isSymmetric(TreeNode root) {
-//
-//    }
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
 
-//    private boolean level(TreeNode root) {
-//
-//        Queue<TreeNode> queue = new ArrayDeque<>();
-//
-//
-//    }
+        return isMirror(root.left, root.right);
+
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+
+    }
 
 }
