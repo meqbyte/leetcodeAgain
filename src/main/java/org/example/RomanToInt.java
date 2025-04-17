e66eef425387d8db72f4218ac838b7b0cbd3716a
meqbyte <meq1986@163.com>
2025-04-17 16:13:22 +0800

    done

--- src/main/java/org/example/RomanToInt.java
+++ src/main/java/org/example/RomanToInt.java
@@ -1,114 +1,35 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class RomanToInt {
     public int romanToInt(String s) {
 
-        int curr = 0;
-        int prev = 0;
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        hashMap.put('I', 1);
+        hashMap.put('V', 5);
+        hashMap.put('X', 10);
+        hashMap.put('L', 50);
+        hashMap.put('C', 100);
+        hashMap.put('D', 500);
+        hashMap.put('M', 1000);
 
         int sum = 0;
-        while (curr < s.length()) {
-
-            if (curr == prev) {
-                curr++;
-                continue;
+        for (int i = 0; i < s.length(); i++) {
+            Character curr = s.charAt(i);
+            if (i + 1 < s.length()) {
+                Character next = s.charAt(i + 1);
+                if (hashMap.get(curr) < hashMap.get(next)) {
+                    sum -= hashMap.get(curr);
+                } else {
+                    sum += hashMap.get(curr);
+                }
+            } else {
+                sum += hashMap.get(curr);
             }
-
-            if (s.charAt(curr) == 'V' && s.charAt(prev) == 'I') {
-                sum += 4;
-                curr++;
-                prev = curr;
-                continue;
-            }
-
-            if (s.charAt(curr) == 'X' && s.charAt(prev) == 'I') {
-                sum += 9;
-                curr++;
-                prev = curr;
-                continue;
-            }
-
-            if (s.charAt(curr) == 'L' && s.charAt(prev) == 'X') {
-                sum += 40;
-                curr++;
-                prev = curr;
-                continue;
-            }
-
-            if (s.charAt(curr) == 'C' && s.charAt(prev) == 'X') {
-                sum += 90;
-                curr++;
-                prev = curr;
-                continue;
-            }
-
-            if (s.charAt(curr) == 'D' && s.charAt(prev) == 'C') {
-                sum += 400;
-                curr++;
-                prev = curr;
-                continue;
-            }
-
-            if (s.charAt(curr) == 'M' && s.charAt(prev) == 'C') {
-                sum += 900;
-                curr++;
-                prev = curr;
-                continue;
-            }
-
-            if (s.charAt(prev) == 'I') {
-                sum += 1;
-                prev++;
-                curr++;
-                continue;
-            }
-
-            if (s.charAt(prev) == 'V') {
-                sum += 5;
-                prev++;
-                curr++;
-                continue;
-            }
-
-            if (s.charAt(prev) == 'X') {
-                sum += 10;
-                prev++;
-                curr++;
-                continue;
-            }
-
-            if (s.charAt(prev) == 'L') {
-                sum += 50;
-                prev++;
-                curr++;
-                continue;
-            }
-
-            if (s.charAt(prev) == 'C') {
-                sum += 100;
-                prev++;
-                curr++;
-                continue;
-            }
-
-            if (s.charAt(prev) == 'D') {
-                sum += 500;
-                prev++;
-                curr++;
-                continue;
-            }
-
-            if (s.charAt(prev) == 'M') {
-                sum += 1000;
-                prev++;
-                curr++;
-                continue;
-            }
-
         }
 
-//        if (prev <) {
-//        }
+        return sum;
 
     }
 }
